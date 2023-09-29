3d2d068762b04d2ce6fdec17797f44c7c59c58c2
Sagar Behere <mail@sagar.se>
2023-09-29 12:05:38 -0700

    Improved copy-assets.py

--- copy-assets.py
+++ copy-assets.py
@@ -10,6 +10,8 @@ import sys
 import io
 import regex as re
 
+# Below regex detects instances of ![Optional Alt text](path/to/image.ext "Optional title")
+# Match group 0 is the whole matched text. Match group 1 is the file path. Match group 2 is the Optional title
 images = re.compile(r'!\[[^\]]*\]\((?P<filename>.*?)(?=\"|\))(?P<optionalpart>\".*\")?\)')
 
 def parse_args():
@@ -19,55 +21,53 @@ def parse_args():
     args = parser.parse_args()
     return (args.origin, args.destination)
 
-def delete_target(destination):
+def recreate_target(destination):
     if os.path.isdir(destination):
         shutil.rmtree(destination)
     else:
         logging.info("DESTINATION folder %s does not exist.", str(destination))
+    destination.mkdir(parents=True, exist_ok=False)
+
+def to_be_published(file):
+    post = frontmatter.load(file)
+    if 'publish' in post.keys():
+        return (post['publish'])
+    else:
+        logging.info("ERROR: File %s has no publish key in frontmatter", str(file))
+        return False
 
 def main():
     pathlib.Path('logs').mkdir(parents=True, exist_ok=True) # Create logs/ dir if it does not exist
     logging.basicConfig(filename='logs/copy-assets.log', filemode='w', encoding='utf-8', level=logging.DEBUG)
-
+    # examples: origin_str = /home/sagar/Documents/notes/ ; destination_str = /home/sagar/sagar.se/static/assets/images/
     (origin_str, destination_str) = parse_args()
     origin = pathlib.Path(origin_str)
     destination = pathlib.Path(destination_str)
-    logging.info("ORIGIN: %s , DESTINATION: %s", origin_str, destination_str)
+    logging.info("ORIGIN: %s , DESTINATION: %s", str(origin), str(destination))
     if not os.path.isdir(origin):
         print("ORIGIN folder does not exist. Aborting!")
         sys.exit(1)
 
     logging.info("DELETING target folder %s", destination_str)
-    delete_target(destination)
-
-    sqlitedbfilename = 'logs/assets.db' #TODO: Avoid hardcoded logs directory here
-    dbconn = sqlite3.connect(sqlitedbfilename) #TODO: Error checking on this function
-
-    dbconn.execute('''CREATE TABLE IF NOT EXISTS assets (id INTEGER UNIQUE NOT NULL PRIMARY KEY AUTOINCREMENT, "from" TEXT NOT NULL, from_title TEXT, "to" TEXT NOT NULL)''')
-    dbconn.execute('''DELETE FROM assets''')
-    dbconn.execute('''DELETE FROM SQLITE_SEQUENCE WHERE name="assets"''') # reset the autoincrement id
-    dbconn.commit()
+    recreate_target(destination)
 
     for file in origin.rglob("*.md"):
-        with open(file, 'r') as f:
-            file_content = f.read()
-            for image in images.finditer(file_content): # image.group(0) is the matched string, image.group(1) is the path/to/image.ext, image.group(2) is the title, if present
-                print(str(file), " --> ", image.group(1))
-
-
-
-    #logging.info("COPYING Obsidian vault to target folder %s", destination_str)
-    #copy_source_to_target(origin, destination)
-
-    #logging.info("CREATING _index.md files")
-    #create_index_files(destination)
-    #root_to_index(destination)
-
-    #logging.info ("PRUNING files with publish != True")
-    #prune_nopublish(destination)
-
-    #logging.info("ADDING dates to frontmatter")
-    #add_frontmatter_date(destination)
+        if to_be_published(file):
+            with open(file, 'r') as f:
+                file_content = f.read()
+                for image in images.finditer(file_content): # image.group(0) is the matched string, image.group(1) is the path/to/image.ext, image.group(2) is the title, if present
+                    full_imagefile_path = pathlib.Path(origin, image.group(1).strip("/"))
+                    # print(str(file), " --> ", str(full_imagefile_path))
+                    if os.path.isfile(full_imagefile_path):
+                        # Copy it over?
+                        logging.info("COPYING %s to %s", str(full_imagefile_path), str(destination))
+                        # TODO: Actually copy it
+                        shutil.copy2(full_imagefile_path, destination)
+                    else:
+                        logging.info("FAILED to copy %s because it does not exist.", str(full_imagefile_path))
+                f.close()
+        else:
+            logging.info("SKIPPING %s because it is not to be published.", str(file))
 
 if __name__ == "__main__":
     main()
--- obsidian-export.sh
+++ obsidian-export.sh
@@ -1,12 +1,14 @@
 #! /bin/sh
 
 OBSIDIAN_VAULT=/home/sagar/Documents/notes
-HUGO_SITE=/home/sagar/sagar.se
+#HUGO_SITE=/home/sagar/sagar.se
+HUGO_SITE=./tmpsite
 
 rm -rf logs/
 #rm -rf $HUGO_SITE/content/notes/* $HUGO_SITE/static/assets/*
-python3 export-files.py $OBSIDIAN_VAULT ./notes
-python3 process-wikilinks.py ./notes
-python3 add-backlinks.py
+python3 export-files.py $OBSIDIAN_VAULT $HUGO_SITE/content/notes
+python3 process-wikilinks.py $HUGO_SITE/content/notes
+#python3 add-backlinks.py
+python3 copy-assets.py $OBSIDIAN_VAULT $HUGO_SITE/static/assets/images/
 #cp -a notes/* $HUGO_SITE/content/notes/
 #cp -a $OBSIDIAN_VAULT/assets $HUGO_SITE/static/
